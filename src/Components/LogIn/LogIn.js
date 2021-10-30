415980bb2a09fcba8d3a44a3e3aad7cbcd72d006
afzal mahmud <afzzalmahmud@gmail.com>
2021-10-31 02:03:50 +0600

    working with logIn form

--- src/Components/LogIn/LogIn.js
+++ src/Components/LogIn/LogIn.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
 /* carousel banner components */
 function LogIn(){
     return(
-        <h2>hello from login form</h2>
+       <Container>
+           <div className="login-body">
+               <label htmlFor="email">E-mail</label>
+               <input type="text" />
+               <label htmlFor="password">E-mail</label>
+               <input type="password" />
+               {/* offer-btn came from OfferCard */}
+               <div className="login-btn d-flex">
+                   <button className='offer-btn'>Log In</button>
+                   <button className='offer-btn'>Google LogIn</button>
+               </div>
+           </div>
+       </Container>
     )
 }
 export default LogIn;

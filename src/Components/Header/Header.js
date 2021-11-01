df19aa785eadf90c2de04fe1a6bb0489cd906226
afzal mahmud <afzzalmahmud@gmail.com>
2021-11-01 01:39:22 -0700

    add logIn and logOut button based on condition

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,8 +6,11 @@ import Container from 'react-bootstrap/Container';
 /* import RootCss from RootCss */
 import './RootCss.css';
 import { Link } from 'react-router-dom';
+import useAuth from '../../Hooks/useAuth';
 /* padding-y utility came from App.css */
 function Header() {
+  /* use logOut based on user from context api */
+  const {user,logOut} = useAuth()
   return (
     <Navbar className='padding-y' collapseOnSelect expand="lg" bg="light" variant="light">
       <Container>
@@ -21,9 +24,17 @@ function Header() {
             <Nav.Link as={Link} to='/membership' >Membership</Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Link as={Link} to='/login'>
+
+            {/* add conditionaly logIn and logOut */}
+            {
+              user.email ? <Button className='primary-background fw-bold btn-border' onClick={logOut}>Log Out</Button>
+
+              :<Nav.Link as={Link} to='/login'>
               <Button className='primary-background fw-bold btn-border'>Log In</Button>
             </Nav.Link>
+
+            }
+
           </Nav>
         </Navbar.Collapse>
       </Container>

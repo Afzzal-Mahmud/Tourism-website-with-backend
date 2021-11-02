e469f521888e512a29f3d6b31a3ac9cabde5e6c7
afzal mahmud <afzzalmahmud@gmail.com>
2021-11-01 22:26:46 -0700

    add orderd data but not add already added data

--- src/Pages/Offer/OfferCard.js
+++ src/Pages/Offer/OfferCard.js
@@ -1,9 +1,49 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import axios from 'axios'
+import { useHistory } from 'react-router-dom';
 import './OfferCard.css'
+import useAuth from '../../Hooks/useAuth';
 /* offer card components */
 function OfferCard({offerObject}){
+    const {user} = useAuth()
     const {image,name,description,price} = offerObject
+    /* seting the user order */
+    const [currentOrderItem,setCurrentOrderItem] = useState([])
+    console.log(currentOrderItem,'ontop')
+    /* setting the status of the uesr */
+    // const [status,setStatus] = useState(false)
+    const history = useHistory()
+
+    function redirectLogIn() {
+        // setStatus(true)
+        history.push('/login') 
+    }
+
+    /* add order to the ui */
+    function addOrder() {
+        const orderItem = offerObject;
+        orderItem.email = user.email
+
+        /* conditionali add item to the database */
+
+        if(currentOrderItem._id != orderItem._id){
+            axios.post('http://localhost:5000/order',orderItem)
+            .then(res => {
+                const newOrder = JSON.parse(res?.config?.data)
+                setCurrentOrderItem(newOrder)
+               /* if it have the id we show this message */
+                if(res.data.insertedId){
+                    alert('Offer add successfully')
+                }
+            })
+            .finally(err => {
+                console.log(err)
+            })
+        }else{
+            alert('you already add this item')
+        }
+
+    }
     return(
     //    <div className="d-flex m-4">
            <div className="card-main">
@@ -24,8 +64,13 @@ function OfferCard({offerObject}){
                    <i class="bi bi-star-fill"></i>
                    </span>
                    <h5>{price} per person</h5>
-                   {/* offer btn came from offer.css and btn-border came from App.css */}
-                   <Link to='/yourcart'><button className='offer-btn btn-border'>Add Offer</button></Link>
+
+                   {
+                       user.email ? <button onClick={addOrder}
+                       className='offer-btn btn-border'>Add Offer</button>
+                      
+                       : <button onClick={redirectLogIn} className='offer-btn btn-border'>Log In To Add</button>
+                   }
                </div>
            </div>
     //    </div>
